d22b472119f00ec8507b1c7a40eed8b8d6b4499c
PJ Oliveira <48339286+paulojmo@users.noreply.github.com>
2020-12-18 09:12:56 -0300

    entity com ForeignKey
    asp .net 5 com entity e as ForeignKey

---
+++ estudo02 - net-5/05 - asp-net-entity-ForeignKey/src/ApplicationInfra/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationBusiness.ParametrosModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationInfra
+{
+    public class Contexto :DbContext
+    {
+       
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated(); 
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Pais> Pais { get; set; }
+        public DbSet<Estado> Estado { get; set; }
+    }
+}
