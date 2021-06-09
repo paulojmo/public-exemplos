cf9e8095c967b5a3d7cdc266d4a322fc82ac5c60
PJ Oliveira <paulojmo@gmail.com>
2021-06-09 10:05:31 -0300

    removidos do projeto

--- estudo04 - net-31/01 - Api/src/Compartilhado/Compartilhado/InterfaceBase/IRepositorio.cs
+++
@@ -1,9 +0,0 @@
-namespace Compartilhado.InterfaceBase
-{
-    /// <summary>
-    /// Interface de marcador para repositórios (camada de acesso a dados).
-    /// </summary>
-    public interface IRepositorio
-    {
-    }
-}
